ea6414eea56feefa2016adb360a168c9088ca65a
Marce <marce735@gmail.com>
2023-07-16 19:32:44 -0500

    Se eliminan lineas de prueba

--- nota_parcial.py
+++ nota_parcial.py
@@ -7,9 +7,3 @@ treinta_porciento=(taller_1 + taller_2 + quiz)/3
 setenta_porciento=parcial
 nota_final=(treinta_porciento*30/100) + (parcial*70/100)
 print(f"la nota de analisis es:{nota_final}")
-
-adnas
-
-asdnlaks
-
-asndlas
